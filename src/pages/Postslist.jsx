d58398748cc5ac840b4bd648a774269ba5b3bdcf
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-04 16:42:50 +0100

    import axios

--- src/pages/Postslist.jsx
+++ src/pages/Postslist.jsx
@@ -1,4 +1,16 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
 const PostsList = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('')
+    })
+
+
     return (
         <h1>Lista Posts</h1>
     )

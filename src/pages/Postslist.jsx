166093eecdd8969dba22cac43c055a18735d25b6
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-04 17:24:31 +0100

    es

--- src/pages/Postslist.jsx
+++ src/pages/Postslist.jsx
@@ -6,13 +6,44 @@ const PostsList = () => {
 
     const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        axios.get('')
-    })
+    const url = import.meta.env.VITE_ENDPOINT_URL;
 
+    useEffect(() => {
+        axios.get(url)
+            .then(res => setPosts(res.data))
+            .catch(err => console.error(err))
+    });
 
     return (
-        <h1>Lista Posts</h1>
+        <>
+            <h1>Lista Posts</h1>
+
+            <div className="container">
+                <div className="row">
+                    {
+                        posts.map((elem) => {
+                            
+                            const { id, title, content, image, tags } = elem;
+
+                            return (
+                                <div className="col-3" key={ id }>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h4 className="card-title">{ title }</h4>
+                                            <p className="card-text">{ tags }</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+
+
+
+        </>
     )
 }
 
